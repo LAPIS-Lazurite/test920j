d19fac3f691e80705503b8d54a4a5ace5ae94964
Eiichi Saito <saito253@dsn.lapis-semi.com>
2016-10-20 22:59:17 +0900

    modified main.rb

--- main.rb
+++ main.rb
@@ -1,20 +1,27 @@
 #! /usr/bin/ruby
 
-print("Please choose a execute form of Production[0] or Complete[1]  => ")
-typ = gets().to_i
-
-
 Dir.chdir "./io_test"
 require "./iotest.rb"
 iotest = Iotest.new()
 $SERIAL_PORT = iotest.setCom()
 p $SERIAL_PORT
-iotest = iotest.alltest()
-Dir.chdir "../."
 
 
-Dir.chdir "./rf_test"
-require "./rftest.rb"
-rftest = Rftest.new()
-rftest.alltest(typ)
-Dir.chdir "../."
+while 1
+    print("Do you continue test? y or n :")
+    rep = gets().to_s
+    if rep =~ /n/ then
+        exit
+    end
+
+    print("Please choose a execute form of Production[0] or Complete[1] :")
+    typ = gets().to_i
+
+    iotest = iotest.alltest()
+    Dir.chdir "../."
+    Dir.chdir "./rf_test"
+    require "./rftest.rb"
+    rftest = Rftest.new()
+    rftest.alltest(typ)
+    Dir.chdir "../."
+end
