2e4454838e8e884f827c5c9f0bc7bd7abb39c847
Eiichi Saito <saito253@dsn.lapis-semi.com>
2017-03-13 17:35:38 +0900

    addtion menu

---
+++ menu.rb
@@ -0,0 +1,27 @@
+#! /usr/bin/ruby
+
+Dir.chdir "./io_test"
+require "./iotest.rb"
+iotest = Iotest.new()
+$SERIAL_PORT = iotest.setCom()
+p $SERIAL_PORT
+Dir.chdir "../rf_test"
+require "./rftest.rb"
+rftest = Rftest.new()
+
+
+while 1
+#   print("Please choose the next action Continue(Enter),Exit(x) :")
+    print("続ける場合は[Enter]を終了する場合[x]を入力してください：")
+    rep = gets().to_s
+    if rep =~ /x/ then
+        exit
+    end
+
+    level = 3 # for debug
+    Dir.chdir "../io_test"
+    iotest.alltest(level)
+
+    Dir.chdir "../rf_test"
+    rftest.menu()
+end
